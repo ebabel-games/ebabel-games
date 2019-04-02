6476498eb918e30f408b88c7fa880407ab4a1da1
Thomas Amar <hello@ebabel.eu>
2019-04-02 21:58:17 +0000

    Remove duplicate HTML since react produces the content of homepage. Fix the footer to separate the 3 pieces of information.

--- src/Footer.js
+++ src/Footer.js
@@ -3,11 +3,10 @@ import React from "react";
 export default () => (
   <footer class="container">
     <p>
-      <abbr title="Zelfstandige Zonder Personeel" lang="nl-nl">ZZP</abbr>
-      Thomas Amar
-      <abbr title="Kamer van Koophandel" lang="nl-nl">KvK</abbr>
-      <span title="Amsterdam Chamber of Commerce registration number">74269100</span>
-      <abbr title="Belasting over de Toegevoegde Waarde" lang="nl-nl">BTW</abbr>
+      <abbr title="Zelfstandige Zonder Personeel" lang="nl-nl">ZZP</abbr>&nbsp;Thomas Amar&nbsp;&bull;&nbsp;
+      <abbr title="Kamer van Koophandel" lang="nl-nl">KvK</abbr>&nbsp;
+      <span title="Amsterdam Chamber of Commerce registration number">74269100</span>&nbsp;&bull;&nbsp;
+      <abbr title="Belasting over de Toegevoegde Waarde" lang="nl-nl">BTW</abbr>&nbsp;
       <span title="Value Added Tax (VAT) number">NL784074070B02</span>
     </p>
   </footer>
