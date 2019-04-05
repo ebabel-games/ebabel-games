b19d2d9f563f282b40aa201074c0e16556dfb778
Thomas Amar <hello@ebabel.eu>
2019-04-05 11:02:05 +0000

    Update the text of Pictologo privacy policy.

--- src/PictologoPrivacyPolicy.js
+++ src/PictologoPrivacyPolicy.js
@@ -10,8 +10,8 @@ export default () => (
     <MainHeader title="Pictologo" subtitle="Privacy Policy" />
     <Nav />
     <section className="container">
-      <ContentBlock title="Pictologo doesn't store any data">
-        <p>The Pictologo game does not store your data. When children play Pictologo, their data stays on the mobile device. No attempt will ever be made to collect any data whatsoever.</p>
+      <ContentBlock title="Pictologo stores no data">
+        <p>The Pictologo game does not store any of your data. When children play Pictologo, their data stays on the mobile device. No attempt will ever be made to collect any data whatsoever.</p>
         <p>If you have any concern about this privacy policy, feel free to <a href="mailto:hello@ebabel.eu">get in touch</a> by e-mail.</p>
         <p>Our Privacy Officer, Thomas Amar, will gladly answer any question you may have.</p>
       </ContentBlock>
